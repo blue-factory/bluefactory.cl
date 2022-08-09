66dd12ae2714232c9415ca1f7bc3d66b1e8d29ad
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-08-09 09:39:21 -0400

    feat: add logotipo and isotipo

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, HStack, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 import LanguageToogle from "./LanguageToogle";
 
@@ -13,6 +14,13 @@ const Header = () => {
       gridGap={2}
       margin="8"
     >
+      <Link to="/">
+        <HStack align="center" justify="center">
+          <Image width={8} src="/assets/bluefactory-isotipo.png" />
+          <Image width={40} src="/assets/bluefactory-logotipo.png" />
+        </HStack>
+      </Link>
+
       <Box marginLeft="auto">
         <LanguageToogle />
       </Box>
--- src/lib/pages/landing/components/SectionThree.tsx
+++ src/lib/pages/landing/components/SectionThree.tsx
@@ -1,4 +1,3 @@
-import { CheckIcon } from "@chakra-ui/icons";
 import {
   Box,
   Container,
@@ -10,42 +9,39 @@ import {
   HStack,
   VStack,
 } from "@chakra-ui/react";
-
-// Replace test data with your own
-// eslint-disable-next-line prefer-spread, func-names
-const features = Array.apply(null, Array(8)).map(function (x, i) {
-  return {
-    id: i,
-    title: "Lorem ipsum dolor sit amet",
-    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.",
-  };
-});
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const SectionThree = () => {
   return (
-    <Box p={4}>
+    <Box p={4} m={4}>
       <Stack spacing={4} as={Container} maxW="3xl" textAlign="center">
-        <Heading fontSize="3xl">This is the headline</Heading>
-        <Text color="gray.600" fontSize="xl">
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua.
-        </Text>
+        <Heading fontSize="3xl">Diseño, software e ingeniería</Heading>
       </Stack>
 
       <Container maxW="6xl" mt={10}>
-        <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
-          {features.map((feature) => (
-            <HStack key={feature.id} align="top">
-              <Box color="green.400" px={2}>
-                <Icon as={CheckIcon} />
-              </Box>
-              <VStack align="start">
-                <Text fontWeight={600}>{feature.title}</Text>
-                <Text color="gray.600">{feature.text}</Text>
-              </VStack>
-            </HStack>
-          ))}
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 2 }}>
+          <HStack padding={["0", "5", "5", "5"]}>
+            <Box color="defiForPeople" px={2}>
+              <Icon as={FavoriteIcon} />
+            </Box>
+            <VStack align="start">
+              <Text color="gray.600" textAlign="justify">
+                adsfkjds a;flkjadsf ;lsdflkajsdf; lkdsaf adsf;lkjdsf iejrasdlj
+              </Text>
+            </VStack>
+          </HStack>
+
+          <HStack pt={["10", "0", "0"]} paddingLeft={["0", "5", "5", "5"]}>
+            <Box color="defiForPeople" px={2}>
+              <Icon as={EmojiEventsIcon} />
+            </Box>
+            <VStack align="start">
+              <Text color="gray.600">
+                aldsfjldaskfj aldsfjkads;flkjdsa ad;lfkj asd
+              </Text>
+            </VStack>
+          </HStack>
         </SimpleGrid>
       </Container>
     </Box>
